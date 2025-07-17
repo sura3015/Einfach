b4fe74031850f7f1705c9439f9cd20d13be314c6
sura <korogi.ryusei@gmail.com>
2025-07-17 21:07:15 +0900

    Update site

--- sw.js
+++ sw.js
@@ -1,22 +1,81 @@
+const CACHE_NAME = "einfach-code-editor-cache-v5";
+const OFFLINE_URL = "./src/offline.html";
+
+const urlsToCache = [
+  "./icon-192.png",
+  "./icon-512.png",
+  OFFLINE_URL,
+  "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined",
+];
+
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches
-      .open("vscode-pwa")
-      .then((cache) =>
-        cache.addAll([
-          "./",
-          "./index.html",
-          "./app.js",
-          "./manifest.json",
-          "./icon-192.png",
-          "./icon-512.png",
-        ])
-      )
+      .open(CACHE_NAME)
+      .then((cache) => {
+        console.log("Opened cache");
+        return cache.addAll(urlsToCache);
+      })
+      .catch((error) => {
+        console.error("Failed to cache during install:", error);
+      })
   );
 });
 
 self.addEventListener("fetch", (event) => {
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request).catch(() => {
+        return caches.match(OFFLINE_URL);
+      })
+    );
+    return;
+  }
+
   event.respondWith(
-    caches.match(event.request).then((res) => res || fetch(event.request))
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request)
+        .then((response) => {
+          if (
+            !response ||
+            response.status !== 200 ||
+            response.type !== "basic"
+          ) {
+            return response;
+          }
+
+          const responseToCache = response.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, responseToCache);
+          });
+          return response;
+        })
+        .catch((error) => {
+          console.warn("Fetch failed for:", event.request.url, error);
+          return new Response(
+            "You are offline and this resource is not cached.",
+            { status: 503, statusText: "Service Unavailable" }
+          );
+        });
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            console.log("Deleting old cache:", cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
