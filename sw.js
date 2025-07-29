c5564b86f7b57210fd1a8f71043dba3450aa0d0e
sura <korogi.ryusei@gmail.com>
2025-07-29 16:02:36 +0900

    UPdate

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "einfach-code-editor-cache-v25";
+const CACHE_NAME = "einfach-code-editor-cache-v26";
 const OFFLINE_PAGE_NAME = "offline.html";
 const getBaseUrlPath = () => {
   const scopePath = self.registration.scope;
